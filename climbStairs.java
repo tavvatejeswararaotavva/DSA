7a14c58e8a84762ec01de81a7bb2506f515bdb04
tavvatejeswararaotavva <tejaswararaotavva@gmail.com>
2025-11-30 15:31:50 +0530

    climb stairs

---
+++ climbStairs.java
@@ -0,0 +1,16 @@
+public class climbStairs {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int a=1,b=2;
+        for(int i =3; i<=n;i++){
+            int c= a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+    public static void main(String[] args) {
+        climbStairs cs = new climbStairs();
+        System.out.println(cs.climbStairs(5));
+    }
+}
